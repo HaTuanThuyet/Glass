9d097f08ea8ce032782c9b1fe9bf477dda680c12
Thuyet <thuyetha25@gmail.com>
2021-10-14 16:29:57 +0700

    firstcommit

---
+++ src/Component/Glass.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react'
+
+export default class Glass extends Component {
+    state = {
+        imgCar: "./Img/v1.png"
+    }
+    renderImg = (color) => {
+        // Gán Giá Trị Mới Cho State,render Lại UI
+        this.setState({
+            imgCar: `./Img/v${color}.png`
+        })
+    }
+    render() {
+        return (
+            <div className="imgmodel opa" style={{ backgroundImage: "url(./img/background.jpg)" }} >
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <img src="./img/model.jpg" alt="" className="img-fluid my-5" style={{marginTop:"100px"}}/>
+                            <img src={this.state.imgCar} alt="" className="img-fluid child" style={{ height: "80px" }} />
+                        </div>
+                    
+
+                    </div>
+                  
+                    <div className="row mx-5 bodyChild ">
+                            <div className="col-2">
+                                <img className="imgchild" src="./img/g1.jpg" onClick={
+                                    () => {
+                                        this.renderImg("1")
+                                    }
+                                }></img>
+                            </div>
+
+
+                            <div className="col-2">
+                                <img className="imgchild" src="./img/g2.jpg" onClick={
+                                    () => {
+                                        this.renderImg("2")
+                                    }
+                                }></img>
+                            </div>
+
+
+                            <div className="col-2">
+                                <img className="imgchild" src="./img/g3.jpg" onClick={
+                                    () => {
+                                        this.renderImg("3")
+                                    }
+                                }></img>
+                            </div>
+
+
+                            <div className="col-2">
+                                <img className="imgchild" src="./img/g4.jpg" onClick={
+                                    () => {
+                                        this.renderImg("4")
+                                    }
+                                }></img>
+                            </div>
+                            <div className="col-2">
+                                <img className="imgchild" src="./img/g5.jpg" onClick={
+                                    () => {
+                                        this.renderImg("5")
+                                    }
+                                }></img>
+                            </div>
+                            <div className="col-2">
+                                <img className="imgchild" src="./img/g6.jpg" onClick={
+                                    () => {
+                                        this.renderImg("6")
+                                    }
+                                }></img>
+                            </div>
+                            <div className="col-2">
+                                <img className="imgchild" src="./img/g7.jpg" onClick={
+                                    () => {
+                                        this.renderImg("7")
+                                    }
+                                }></img>
+                            </div>
+                            <div className="col-2">
+                                <img className="imgchild" src="./img/g8.jpg" onClick={
+                                    () => {
+                                        this.renderImg("8")
+                                    }
+                                }></img>
+                            </div>
+                            <div className="col-2">
+                                <img className="imgchild" src="./img/g9.jpg" onClick={
+                                    () => {
+                                        this.renderImg("9")
+                                    }
+                                }></img>
+                            </div>
+
+
+
+
+
+
+                        </div>
+
+                  
+
+
+
+
+
+
+
+                </div>
+            </div>
+
+
+
+
+
+        )
+    }
+}
